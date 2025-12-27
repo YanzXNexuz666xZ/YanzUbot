ae226dad5f1022ec175dd2488b0d37fd06e2b551
YanzXNexuz666xZ <bibityasin49@gmail.com>
2025-12-27 19:19:11 +0700

    Add files via upload

---
+++ userbot.py
@@ -0,0 +1,4 @@
+# Dapatkan dari https://my.telegram.org
+API_ID = 38528762  # Ganti dengan API ID Anda
+API_HASH = "6f7b6aa1197fdcb8d35106a4d63abc88"  # Ganti dengan API Hash Anda
+SESSION_NAME = "my_userbot"
