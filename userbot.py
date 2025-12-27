30fada5236816d6631ae4417214ffc035c933c21
YanzXNexuz666xZ <bibityasin49@gmail.com>
2025-12-27 22:22:19 +0700

    Delete userbot.py

--- userbot.py
+++
@@ -1,4 +0,0 @@
-# Dapatkan dari https://my.telegram.org
-API_ID = 38528762  # Ganti dengan API ID Anda
-API_HASH = "6f7b6aa1197fdcb8d35106a4d63abc88"  # Ganti dengan API Hash Anda
-SESSION_NAME = "my_userbot"
